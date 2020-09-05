628b118dae531bd48fd8110353ef9bb9dbbda0a1
themikkoz <mikko.jyrkas@hotmail.com>
2020-09-06 01:48:47 +0300

    Added GUI

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import youtube_dl
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Youtube downloader")
 
 ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
 
@@ -9,22 +13,23 @@ def clear():
     else:
         _ = os.system('clear')
 
-def downloadMP4(url):
-    with ydl:
-        result = ydl.extract_info(
-            url,
-            download=True
-        )
+def geturl():
+    downloadMP4(userInput.get())
 
-if __name__ == "__main__":
-    run = True
-    while run:
-        uInput = input()
-        if uInput == "quit":
-            quit()
-        try:
-            downloadMP4(uInput)
-            clear()
-            print("Downloaded successfully")
-        except:
-            print("!ERROR!")
+def downloadMP4(url):
+    try:
+        with ydl:
+            result = ydl.extract_info(
+                url,
+                download=True
+            )
+    except:
+        pass
+
+tk.Label(root, text="Url: ").grid(row=0)
+userInput = tk.Entry(root, width=100)
+userInput.grid(row=0, column=1)
+
+tk.Button(root, text="Download", command=geturl).grid(row = 2)
+
+root.mainloop()
