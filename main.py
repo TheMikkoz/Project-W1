dec56e439134880c7b812d99bdec7371555d8b17
themikkoz <mikko.jyrkas@hotmail.com>
2020-09-06 23:06:21 +0300

    Added TkInter

--- main.py
+++ main.py
@@ -23,8 +23,24 @@ def downloadMP4(url):
                 url,
                 download=True
             )
+
+        done()
     except:
-        pass
+        error()
+
+def error():
+    popup = tk.Tk()
+    popup.wm_title("ERROR")
+    label = tk.Label(popup, text=("Url: " + userInput.get() + " was not valid")).grid(row=1)
+    B1 = tk.Button(popup, text="Okay", command = popup.destroy).grid(row=2)
+    popup.mainloop()
+
+def done():
+    popup = tk.Tk()
+    popup.wm_title("Done")
+    label = tk.Label(popup, text="Downloaded successfully").grid(row=1)
+    B1 = tk.Button(popup, text="Okay", command = popup.destroy).grid(row=2)
+    popup.mainloop()
 
 tk.Label(root, text="Url: ").grid(row=0)
 userInput = tk.Entry(root, width=100)
